a10e1310be935f9f02d375ec3b761a8b860fad1e
Chukwuneku <121660030+nekumartins@users.noreply.github.com>
2023-03-10 06:25:46 +0100

    more lines of code

--- peaches.py
+++ peaches.py
@@ -12,4 +12,16 @@ async def on_ready():
     print('Bot is online')
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello there, did you wash your ass today')
+        print(f"Message received: {message.content}")
+
+
+
+
 client.run(TOKEN)
