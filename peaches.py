9ee01ff9a5ebcc0961db00393bda58afefa27b3d
Chukwuneku <121660030+nekumartins@users.noreply.github.com>
2023-03-10 22:24:11 +0100

    made environment variable

--- peaches.py
+++ peaches.py
@@ -1,27 +1,33 @@
 import discord 
-import random 
 
-TOKEN ='MTA4MzMxNTA1MjM5Mzc5OTcyMA.GNDhqk.Rhtc3mnycWIycNU1ra42BkfPD2wi79FCpqqpDQ'
+
+TOKEN = 'MTA4MzMxNTA1MjM5Mzc5OTcyMA.GN8bt9.2dFCXgcNGEl9M_FhMIOGdJuCDtHmA0kjhPr-PI'
 
 intents = discord.Intents.default()
+intents.messages = True
 client = discord.Client(intents=intents)
 
 
 @client.event 
 async def on_ready():
-    print('Bot is online')
+    print('logged in as {0.user}'.format(client))
+
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    if message.content.startswith('$greet'):
+        channel = message.channel
+        await channel.send('Say hello!')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello there, did you wash your ass today')
-        print(f"Message received: {message.content}")
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send(f'Hello {msg.author}!')
 
 
 
 
 client.run(TOKEN)
+print("Bot has terminated")
