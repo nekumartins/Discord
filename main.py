95cbb51d136ec40dddc77fc04d1d6cebb9c3be6e
Chukwuneku <121660030+nekumartins@users.noreply.github.com>
2023-03-11 01:32:20 +0100

    updated logging

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+import settings
 import discord 
+from discord.ext import commands
+
+logger = settings.logging.getLogger("bot")
+
+def run():
+    intents = discord.Intents.default()
+
+    bot = commands.Bot(command_prefix='#', intents=intents)
+
+    @bot.event
+    async  def on_ready():
+        logger.info(f"User: {bot.user}")
+        
+    bot.run(settings.SECRET, root_logger=True) 
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    run()
+    
 
 
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import logging
+from logging.config import dictConfig 
 from dotenv import load_dotenv
 import os 
 
@@ -6,5 +8,55 @@ import os
 load_dotenv()
 
 SECRET= os.getenv("TOKEN")
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_Loggers": False,
+    "formatters":{
+        "verbose":{
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard":{
+            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
+        }
+    },
+    "handlers":{
+        "console":{
+            'level': "DEBUG",
+            'class': "logging.StreamHandler",
+            'formatter' : "standard"        
+        },
+        "console2":{
+            'level': "WARNING",
+            'class': "logging.StreamHandler",
+            'formatter' : "standard"        
+        },
+        "file":{
+            'level': "INFO",
+            'class': "logging.FileHandler",
+            'filename': "Logs/infos.Log",
+            'mode' : "w",
+            'formatter' : "verbose"
+        },
+    
+    },
+    "loggers":{
+    "bot": {
+        'handlers' : ['console'],
+        "level" : "INFO",
+        "propagate" : False
+    },
+    "discord": {
+        'handlers' : ['console2', "file"],
+        "level" : "INFO",
+        "propagate" : False
+    
+    },        
+    }
+
+}
+
+dictConfig(LOGGING_CONFIG)
 
 
