e1a06b78d8952d118cc8bb1de04b67ba1fcb0b75
Chukwuneku <121660030+nekumartins@users.noreply.github.com>
2023-03-11 02:14:50 +0100

    created new.env file

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ logger = settings.logging.getLogger("bot")
 
 def run():
     intents = discord.Intents.default()
+    intents.message_content = True
 
     bot = commands.Bot(command_prefix='#', intents=intents)
 
     @bot.event
     async  def on_ready():
         logger.info(f"User: {bot.user}")
+
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send("pong")
         
     bot.run(settings.SECRET, root_logger=True) 
 
